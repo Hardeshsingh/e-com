aac3199fdc1fff05766c189d91ba619084ec2b2e
Hardeshsingh <132184357+Hardeshsingh@users.noreply.github.com>
2023-07-13 14:16:58 +0530

    code

---
+++ E-com/index.js
@@ -0,0 +1,10 @@
+var navbarItems = document.getElementsByClassName("navbar-item");
+for (var i = 0; i < navbarItems.length; i++) {
+  navbarItems[i].addEventListener("click", function() {
+    var currentActive = document.getElementsByClassName("active");
+    if (currentActive.length > 0) {
+      currentActive[0].classList.remove("active");
+    }
+    this.classList.add("active");
+  });
+}
